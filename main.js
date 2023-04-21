f10c755ad8ffb631ff100da5228ff6ca1ccd7a8f
makhmudoff <gachon031698@gmail.com>
2023-04-21 23:51:05 +0900

    deleted btn function from  main.js

--- main.js
+++ main.js
@@ -1,35 +1,6 @@
 'use strict';
 
-const item = document.getElementById('box');
-const listItem = document.getElementById('list_item');
-const btn = document.getElementById('addItem');
-
-btn.addEventListener('click', () => {
-  if (item.value !== '') {
-    // Creating ol element and adding value to it
-    let make_ol = document.createElement('ol');
-    make_ol.appendChild(document.createTextNode(item.value));
-    let deleteButton = document.createElement('button');
-    deleteButton.innerHTML = 'Delete';
-    // adding ol to ul
-
-    listItem.appendChild(make_ol);
-    listItem.appendChild(deleteButton);
-    // reset the value of box
-    item.value = '';
-
-    // delete a il item on click
-
-    make_ol = function () {
-      this.appendNode.removeChild(this);
-    };
-
-    //
-    deleteButton.addEventListener('click', function () {
-      listItem.remove();
-    });
-  } else {
-    // Alert msg when value of box is ' ' empty
-    alert('Please add a value to item');
-  }
-});
+const inputItem = document.getElementById('box');
+const listItem = document.querySelector('list_item');
+const addItem = document.querySelector('addItem');
+const deleteItem = document.querySelector('deleteItem');
