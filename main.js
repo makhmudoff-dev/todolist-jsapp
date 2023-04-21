ded404559f80f30f9edd5b9ad0422441b22261f3
makhmudoff <gachon031698@gmail.com>
2023-04-21 23:53:08 +0900

    added delete function

--- main.js
+++ main.js
@@ -3,4 +3,3 @@
 const inputItem = document.getElementById('box');
 const listItem = document.querySelector('list_item');
 const addItem = document.querySelector('addItem');
-const deleteItem = document.querySelector('deleteItem');
